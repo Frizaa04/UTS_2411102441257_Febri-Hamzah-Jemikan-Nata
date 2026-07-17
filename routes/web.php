43343bf16111a377bacd69ff4ed2d9be1dd54722
Frizaa04 <bonedream2@gmail.com>
2026-07-17 15:27:55 +0800

    laravel lupa kemaren

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,30 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\HomeController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.admin.index');
 });
+
+
+// ADMIN
+Route::middleware(['auth', 'admin'])->prefix('admin')->group(function () {
+
+    //Halaman Dashboard Admin (Grafik)
+    Route::get('index', function () {
+        return view('pages.admin.index'); 
+    })->name('admin.index');
+
+   Route::resource('users', UserController::class)->names([
+        'index'   => 'admin.users.index',
+        'create'  => 'admin.users.create',
+        'store'   => 'admin.users.store',
+        'edit'    => 'admin.users.edit',
+        'update'  => 'admin.users.update',
+        'destroy' => 'admin.users.destroy',
+    ]);
+
+    });
